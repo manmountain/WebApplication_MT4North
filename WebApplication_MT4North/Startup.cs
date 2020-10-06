4009a35b543e8ec9e23d6113e6446339a5659e34
dhanamalar <71325187+dhanamalar@users.noreply.github.com>
2020-10-06 13:50:36 +0200

    3rd gånger

--- WebApplication_MT4North/Startup.cs
+++ WebApplication_MT4North/Startup.cs
@@ -67,7 +67,7 @@ namespace WebApplication_MT4North
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }

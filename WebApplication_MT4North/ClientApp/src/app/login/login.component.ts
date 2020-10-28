90dfc346dd6ca224cc52a379cdb86540fd986da4
dhanamalar <71325187+dhanamalar@users.noreply.github.com>
2020-10-28 11:41:13 +0100

    logga in component-grund 1

--- WebApplication_MT4North/ClientApp/src/app/login/login.component.ts
+++ WebApplication_MT4North/ClientApp/src/app/login/login.component.ts
@@ -1,8 +1,11 @@
+import { sharedStylesheetJitUrl } from '@angular/compiler';
 import { Component} from '@angular/core';
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html'
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+ 
 })
 export class LoginComponent {
 

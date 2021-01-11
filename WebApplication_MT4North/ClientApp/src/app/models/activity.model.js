e7b1191c6f886ce271166013f494a36193c10265
Karolina Jonzén <Karolina.Jonzen@regionvasterbotten.se>
2021-01-11 16:57:19 +0100

    Lagt till funktionalitet för Exkludera.

--- WebApplication_MT4North/ClientApp/src/app/models/activity.model.js
+++ WebApplication_MT4North/ClientApp/src/app/models/activity.model.js
@@ -3,11 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Activity = void 0;
 var common_1 = require("./common");
 var Activity = /** @class */ (function () {
-    function Activity(name, description, phase) {
+    function Activity(name, description, phase, isExcluded) {
         this.name = name;
         this.description = description;
         this.phase = phase;
         this.status = common_1.Status.NOTSTARTED;
+        this.isExcluded = isExcluded;
     }
     return Activity;
 }());
--- WebApplication_MT4North/ClientApp/src/app/my-pages-activity-status/my-pages-activity-status.component.ts
+++ WebApplication_MT4North/ClientApp/src/app/my-pages-activity-status/my-pages-activity-status.component.ts
@@ -3,7 +3,7 @@ import { Theme } from "../models/theme.model";
 import { Activity } from "../models/activity.model";
 import { Phase } from "../models/common";
 import { Status } from "../models/common";
-/*import { Console } from 'console';*/
+import { Console } from 'console';
 
 @Component({
   selector: 'app-my-pages-activity-status',
@@ -19,10 +19,12 @@ export class MyPagesActivityStatusComponent {
   }
 
   ngOnInit() {
+    console.log("initiating");
+
     let technicalDevelopmentTheme = new Theme("Teknikutveckling", "Beskrivning");
-    let activity1 = new Activity("Observera grundläggande principer", "Utveckla ett grundläggande koncept som svarar mot behov.", Phase.CONCEPTUALIZATION);
-    let activity2 = new Activity("Genomför kravanalys", "Genomför en kravanalys för konceptet. Finns standarder att förhålla sig till?", Phase.CONCEPTUALIZATION);
-    let activity3 = new Activity("Utveckla prototyp baserat på koncept", "En första prototyp ska utvecklas som kan användas både som proof-of-concept och för att genomföra nödvändiga användartester i detta skede.", Phase.PROOFOFCONCEPT);
+    let activity1 = new Activity("Observera grundläggande principer", "Utveckla ett grundläggande koncept som svarar mot behov.", Phase.CONCEPTUALIZATION, false);
+    let activity2 = new Activity("Genomför kravanalys", "Genomför en kravanalys för konceptet. Finns standarder att förhålla sig till?", Phase.CONCEPTUALIZATION, true);
+    let activity3 = new Activity("Utveckla prototyp baserat på koncept", "En första prototyp ska utvecklas som kan användas både som proof-of-concept och för att genomföra nödvändiga användartester i detta skede.", Phase.PROOFOFCONCEPT, false);
     technicalDevelopmentTheme.addActivity(activity1);
     technicalDevelopmentTheme.addActivity(activity2);
     technicalDevelopmentTheme.addActivity(activity3);
@@ -30,9 +32,9 @@ export class MyPagesActivityStatusComponent {
     this.themes.push(technicalDevelopmentTheme);
 
     let clinicalDevelopmentTheme = new Theme("Klinisk utveckling", "Beskrivning");
-    let activity4 = new Activity("Kontakta klinisk personal", "Kontakta klinisk personal inom relevant område för att bekräfta att produktidén svarar mot ett faktiskt, upplevt behov inom vården, alternativt innebär en för dem relevant förbättring av en existerande lösning. Undersök möjlighet till samarbeten för framtida klinisk utvärdering av prototyp/produkt.", Phase.CONCEPTUALIZATION);
-    let activity5 = new Activity("Genomför klinisk riskanalys", "Genomför en riskanalys i enlighet med relevant standard, tillsammans med personer med nödvändig klinisk kompetens.", Phase.PROOFOFCONCEPT);
-    let activity6 = new Activity("Genomför kliniska tester i laboratoriemiljö", "Testa att prototypen uppfyller de tänkta kliniska grundfunktionerna i relevant laboratoriemiljö.", Phase.PROOFOFCONCEPT);
+    let activity4 = new Activity("Kontakta klinisk personal", "Kontakta klinisk personal inom relevant område för att bekräfta att produktidén svarar mot ett faktiskt, upplevt behov inom vården, alternativt innebär en för dem relevant förbättring av en existerande lösning. Undersök möjlighet till samarbeten för framtida klinisk utvärdering av prototyp/produkt.", Phase.CONCEPTUALIZATION, false);
+    let activity5 = new Activity("Genomför klinisk riskanalys", "Genomför en riskanalys i enlighet med relevant standard, tillsammans med personer med nödvändig klinisk kompetens.", Phase.PROOFOFCONCEPT, false);
+    let activity6 = new Activity("Genomför kliniska tester i laboratoriemiljö", "Testa att prototypen uppfyller de tänkta kliniska grundfunktionerna i relevant laboratoriemiljö.", Phase.PROOFOFCONCEPT, false);
     clinicalDevelopmentTheme.addActivity(activity4);
     clinicalDevelopmentTheme.addActivity(activity5);
     clinicalDevelopmentTheme.addActivity(activity6);
@@ -42,14 +44,14 @@ export class MyPagesActivityStatusComponent {
   }
 
   getProgress(theme: Theme, phase: Phase): number {
-    let activityVal = 100 / theme.activities.filter(x => x.phase == phase).length;
-    let nrOfStarted = theme.activities.filter(x => x.status != Status.NOTSTARTED && x.phase == phase).length;
+    let activityVal = 100 / theme.activities.filter(x => x.phase == phase && x.isExcluded == false).length;
+    let nrOfStarted = theme.activities.filter(x => x.status != Status.NOTSTARTED && x.phase == phase && x.isExcluded == false).length;
 
     return activityVal * nrOfStarted;
   }
 
   containsOngoingAcitvities(theme: Theme, phase: Phase): boolean {
-    return theme.activities.filter(x => x.phase == phase && x.status == Status.ONGOING).length > 0;
+    return theme.activities.filter(x => x.phase == phase && x.status == Status.ONGOING && x.isExcluded == false).length > 0;
   }
 
   checkStatus(activity: Activity) {
@@ -67,5 +69,9 @@ export class MyPagesActivityStatusComponent {
         break;
       }
     }
+  }
+
+  toggleActivityIsExcluded(activity: Activity) {
+    activity.isExcluded = !activity.isExcluded;
   }
 }

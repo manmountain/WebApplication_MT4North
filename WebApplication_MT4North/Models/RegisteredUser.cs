02434f00f38b1f72c7748f635cd2bd3f8b52dfb0
Fredrik <fredrik.ohberg@anymo.se>
2021-02-08 16:29:38 +0100

    Lagt till databaser

---
+++ WebApplication_MT4North/Models/RegisteredUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_MT4North.Models
+{
+    public class RegisteredUser
+    {
+        public int RegisteredUserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAdress { get; set; }
+    }
+}
